da47ead9ce0eb5fd67bc213c66df9888a7a95dd0
Z-Den <gwnkjell@mail.ru>
2023-11-23 14:43:07 +0300

    Update FixedRaycast.cs

--- ARtestProject/Assets/Script/FixedRaycast.cs
+++ ARtestProject/Assets/Script/FixedRaycast.cs
@@ -6,16 +6,11 @@ using UnityEngine;
 
 public class FixedRaycast : MonoBehaviour
 {
-    [SerializeField] private GameObject _rightArm;
-    [SerializeField] private GameObject _leftArm;
-
     private void FixedUpdate()
     {
-        _rightArm = GameObject.Find("Right_ShellHandRayPointer(Clone)");
-        _leftArm = GameObject.Find("Left_ShellHandRayPointer(Clone)");
-        _rightArm.GetComponent<MixedRealityLineRenderer>().enabled = false;
-        _rightArm.GetComponent<ShellHandRayPointer>().enabled = false;
-        _leftArm.GetComponent<MixedRealityLineRenderer>().enabled = false;
-        _leftArm.GetComponent<ShellHandRayPointer>().enabled = false;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<MixedRealityLineRenderer>()) transform.GetChild(i).gameObject.SetActive(false);
+        }
     }
 }
